f5e0b94f7b02b3952b9e97bc4c8ff080f05b377b
aryajason <aryajasonr@gmail.com>
2024-10-15 11:43:00 +0700

    Update mainTest.cpp

--- Struct Data/mainTest.cpp
+++ Struct Data/mainTest.cpp
@@ -1,2 +1,25 @@
-test sementar
-a
+#include <iostream>
+using namespace std;
+struct StudentData {
+    string NIM;
+    float UTS;
+    float UAS;
+    float avg;
+} ;
+
+StudentData NumberStudent[50];
+int main() 
+{
+NumberStudent[0] .NIM = "22780/PA" ;
+NumberStudent[0] .UTS = 90 ;
+NumberStudent[0] .UAS = 83 ;
+
+NumberStudent[0].avg = (NumberStudent[0].UTS + NumberStudent[0].UAS)/2 ;
+
+string expectedNIM = "22780/PA" ;
+float expectedavg = (90+83)/2 ;
+
+if (expectedNIM == NumberStudent[0].NIM && expectedAvg == NumberStudent[0].avg) {
+        cout << "Test Passed: SUCCESSFUL" << '\n';
+ }
+}
