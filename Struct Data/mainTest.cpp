0e931b1e038409a00972203c1d0017d48541268d
aryajason <aryajasonr@gmail.com>
2024-10-15 11:49:05 +0700

    Update mainTest.cpp

--- Struct Data/mainTest.cpp
+++ Struct Data/mainTest.cpp
@@ -17,7 +17,7 @@ NumberStudent[0] .UAS = 83 ;
 NumberStudent[0].avg = (NumberStudent[0].UTS + NumberStudent[0].UAS)/2 ;
 
 string expectedNIM = "22780/PA" ;
-float expectedavg = (90+83)/2 ;
+float expectedAvg = (90+83)/2 ;
 
 if (expectedNIM == NumberStudent[0].NIM && expectedAvg == NumberStudent[0].avg) {
         cout << "Test Passed: SUCCESSFUL" << '\n';
